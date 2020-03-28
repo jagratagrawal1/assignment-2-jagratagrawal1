334af2a14a911aee96f285fe8cd198a2fe81514a
jagratagrawal1 <jagrat.agrawal_cs18@gla.ac.in>
2020-03-28 21:27:21 +0530

    program is done and executed properly.

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,37 @@
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode leftNode;
+    private TreeNode rightNode;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    @Override
+    public String toString() {
+        return "data ==>> " + getData();
+    }
+
 }
